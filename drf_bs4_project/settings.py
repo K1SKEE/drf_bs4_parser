a5d6d72d46f40b6e46e0d3cf6b19cef269549ed6
K1SKEE <wenzelx.valve@gmail.com>
2023-01-01 14:29:38 +0200

    updated database settings and requirements.txt

--- drf_bs4_project/settings.py
+++ drf_bs4_project/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
-
 from pathlib import Path
+from getenv import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY' or 'empty')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +77,12 @@ WSGI_APPLICATION = 'drf_bs4_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('BASE_NAME' or 'empty'),
+        'USER': env('BASE_USER' or 'empty'),
+        'PASSWORD': env('BASE_PASSWORD' or 'empty'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
