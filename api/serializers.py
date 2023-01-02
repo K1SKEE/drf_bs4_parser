30cd432dc54de251d74ea065af754bd4e29cb80d
K1SKEE <wenzelx.valve@gmail.com>
2023-01-02 12:54:44 +0200

    created serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,35 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from .models import Vacancy
+from .utils import parse_vacancies
 
 
-class VacancyCreateSerializer(serializers.ModelSerializer):
-    pass
+class VacancyParseSerializer(serializers.Serializer):
+    url = serializers.URLField()
+    parse_pages = serializers.IntegerField()
+
+    def create(self, validated_data):
+        vacancies = parse_vacancies(
+            link=validated_data.get('url'),
+            parse_pages=validated_data.get('parse_pages')
+        )
+        result = []
+        for vacancy in vacancies:
+            try:
+                href = vacancy.get('href')
+                Vacancy.objects.get(pk=href)
+            except ObjectDoesNotExist:
+                result.append(Vacancy.objects.create(vacancy))
+        return result
 
 
 class VacancyListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacancy
-        fields = ('url', 'title', )
+        exclude = ('description', 'parse_date')
+
+
+class VacancyDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
